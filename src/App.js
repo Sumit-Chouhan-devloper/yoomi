5b6638dce24f89292a3d26f4dd799d0ce92309e4
arunkuma <neharadialcode@gmail.com>
2023-10-03 12:38:18 +0530

    enums added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Hero from "./components/Hero.tsx";
+import { routes } from "./enums";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Hero />} />
+        <Route path={routes.HOME} element={<Hero />} />
       </Routes>
     </BrowserRouter>
   );
