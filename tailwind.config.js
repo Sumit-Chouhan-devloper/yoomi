d2bda8ff4bf03e9b344a8d64e43ad9b8b66b113d
mk0029 <mkhicher404@gmail.com>
2023-10-03 20:07:33 +0530

    add root

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        xxs: "11px",
+        xs: "14px",
+        sm: "16px",
+        lg: "18px",
+        xl: "20px",
+        "2xl": "24px",
+        "3xl": "28px",
+        "4xl": "30px",
+        "5xl": "33px",
+        "6xl": "40px",
+        "7xl": "50px",
+        "8xl": "52px",
+        "9xl": "60px",
+      },
+      colors: {
+        white: "#ffffff",
+        "dark-blue": "#110163",
+        purple: "#81488e",
+        "dark-gray": "#303030",
+        "light-gray": "#6a6969",
+        "light-pink": "#fbe8ff",
+        blue: "#431ea6",
+        green: "#85b6a6",
+        "light-purple": "#9d8fcd",
+      },
+    },
   },
   plugins: [],
 };
