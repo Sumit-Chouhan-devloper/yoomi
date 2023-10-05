6205b223cb82a375235e4056ec4835f033539a77
Your Name <sumitkhan.dhana@gmail.com>
2023-10-05 16:26:33 +0530

    feature update

--- src/view/Main.tsx
+++ src/view/Main.tsx
@@ -4,11 +4,13 @@ import Header from "../components/homepage/Header.tsx";
 import TreakingRemote from "../components/homepage/TreakingRemote.tsx";
 import ReportingAnylats from "../components/homepage/ReportingAnylats.tsx";
 import TrackingFeedback from "../components/homepage/TreakingFeedback.tsx";
+import Feature from "../components/homepage/Feature.tsx";
 
 const Main = () => {
   return (
     <>
       <Header />
+      <Feature/>
       <TreakingRemote />
       <ReportingAnylats />
       <TrackingFeedback />
