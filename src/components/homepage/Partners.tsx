173735ced3b2f838ef332506daa4799c43c54007
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-10-07 21:08:20 +0530

    chng in partners

--- src/components/homepage/Partners.tsx
+++ src/components/homepage/Partners.tsx
@@ -5,19 +5,18 @@ const Partners = () => {
   return (
     <>
       <div className="container xl:max-w-[1140px] mx-auto px-5 xl:px-0 text-center sm:py-10 py-[30px] lg:pb-[113px] lg:pt-[118px]">
-        <p
-          className="text-deep-blue text-xs sm:text-sm font-medium uppercase pt-5 sm:pt-0"
-          >
+        <p className="text-deep-blue text-xs sm:text-sm font-medium uppercase pt-5 sm:pt-0">
           Partners
         </p>
-        <p
-          className="text-black ff_Jakarta text-[20px] sm:text-6xl font-normal mb-2 lg:px-40 leading-[120%]"
-          >
+        <p className="text-black ff_Jakarta text-[20px] sm:text-6xl font-normal mb-2 lg:px-40 leading-[120%]">
           Recognized by Leading
           <span className="font-semibold"> Healthcare Institutions</span>
         </p>
         <div className="flex justify-center min-[1025px]:justify-between] mt-4 sm:mt-[55px] gap-5 flex-wrap">
-          <div className="mt-2sm:mt-4 flex justify-center items-center w-[150px] sm:w-[207px] h-[110px] sm:h-[137px] rounded-[4px] border-solid border-[1px] border-[rgba(0, 0, 0, 0.10)] shadow-lg shadow-[0px_13px_18px_0px_rgba(0, 0, 0, 0.06)] bg-white hover:shadow-[unset] duration-300 ease-linear">
+          <div
+            className="mt-2sm:mt-4 flex justify-center items-center w-[150px] sm:w-[207px] h-[110px] sm:h-[137px] rounded-[4px] border-solid border-[1px] border-[rgba(0, 0, 0, 0.10)] shadow-lg shadow-[0px_13px_18px_0px_rgba(0, 0, 0, 0.06)] bg-white hover:shadow-[unset] duration-300 ease-linear"
+            data-aos="zoom-in"
+          >
             <img
               src={Images.partnerIconOne}
               alt="partner-icon-1"
@@ -26,7 +25,8 @@ const Partners = () => {
           </div>
           <div
             className="mt-2sm:mt-4 flex justify-center items-center w-[150px] sm:w-[207px] h-[110px] sm:h-[137px] rounded-[4px] border-solid border-[1px] border-[rgba(0, 0, 0, 0.10)] shadow-lg shadow-[0px_13px_18px_0px_rgba(0, 0, 0, 0.06)] bg-white hover:shadow-[unset] duration-300 ease-linear"
-            data-aos="zoom-in">
+            data-aos="zoom-in"
+          >
             <img
               src={Images.partnerIconTwo}
               alt="partner-icon-1"
@@ -35,7 +35,8 @@ const Partners = () => {
           </div>
           <div
             className="mt-2sm:mt-4 flex justify-center items-center w-[150px] sm:w-[207px] h-[110px] sm:h-[137px] rounded-[4px] border-solid border-[1px] border-[rgba(0, 0, 0, 0.10)] shadow-lg shadow-[0px_13px_18px_0px_rgba(0, 0, 0, 0.06)] bg-white hover:shadow-[unset] duration-300 ease-linear"
-            data-aos="zoom-in">
+            data-aos="zoom-in"
+          >
             <img
               src={Images.partnerIconThree}
               alt="partner-icon-1"
@@ -44,7 +45,8 @@ const Partners = () => {
           </div>
           <div
             className="mt-2sm:mt-4 flex justify-center items-center w-[150px] sm:w-[207px] h-[110px] sm:h-[137px] rounded-[4px] border-solid border-[1px] border-[rgba(0, 0, 0, 0.10)] shadow-lg shadow-[0px_13px_18px_0px_rgba(0, 0, 0, 0.06)] bg-white hover:shadow-[unset] duration-300 ease-linear"
-            data-aos="zoom-in">
+            data-aos="zoom-in"
+          >
             <img
               src={Images.partnerIconFour}
               alt="partner-icon-1"
@@ -53,7 +55,8 @@ const Partners = () => {
           </div>
           <div
             className="mt-2sm:mt-4 flex justify-center items-center w-[150px] sm:w-[207px] h-[110px] sm:h-[137px] rounded-[4px] border-solid border-[1px] border-[rgba(0, 0, 0, 0.10)] shadow-lg shadow-[0px_13px_18px_0px_rgba(0, 0, 0, 0.06)] bg-white hover:shadow-[unset] duration-300 ease-linear"
-            data-aos="zoom-in">
+            data-aos="zoom-in"
+          >
             <img
               src={Images.partnerIconFive}
               alt="partner-icon-1"
