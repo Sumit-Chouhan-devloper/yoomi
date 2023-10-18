e77765e7c3b6b6b0522e2a43f73963583a0f2ba7
sukhvinder-jaat <monudhaka829@gmail.com>
2023-10-18 15:51:42 +0530

    hero complete

--- src/view/ContactUs.tsx
+++ src/view/ContactUs.tsx
@@ -3,10 +3,12 @@ import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
 import SignUp from "../components/common/SignUp";
 import ContactForm from "../components/contactUs/ContactForm";
+import ContactHero from "../components/contactUs/ContactUsHero";
 const ContactUs: React.FC = () => {
   return (
     <>
       <Header />
+      <ContactHero />
       <ContactForm />
       <SignUp />
       <Footer />
