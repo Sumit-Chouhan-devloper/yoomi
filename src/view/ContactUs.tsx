870d040b9c574087273f48169356b4d9fe6f0367
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-10-18 10:02:41 +0530

    rename contact form section

--- src/view/ContactUs.tsx
+++ src/view/ContactUs.tsx
@@ -3,7 +3,7 @@ import CommonHero from "../components/common/CommonHero.tsx";
 import Header from "../components/common/Header.tsx";
 import Footer from "../components/common/Footer.tsx";
 import SignUp from "../components/common/SignUp.tsx";
-import UpdatedUs from "../components/contactUs/UpdatedUs.tsx";
+import ContactForm from "../components/contactUs/ContactForm.tsx";
 const ContactUs = () => {
   return (
     <>
@@ -17,7 +17,7 @@ const ContactUs = () => {
           "Hoping to have a question answered? Curious about using Yoomi for your practice? Reach out to our team for a quick response!"
         }
       />
-      <UpdatedUs />
+      <ContactForm/>
       <SignUp />
       <Footer />
     </>
