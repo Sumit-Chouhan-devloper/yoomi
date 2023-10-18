3672bf0e0ab3493838d1dfe97f0d49463d08cd45
anjali-chanda-70565 <anjalichanda2019@gmail.com>
2023-10-18 09:33:53 +0530

    cmnt updated us

--- src/view/ContactUs.tsx
+++ src/view/ContactUs.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CommonHero from "../components/common/CommonHero.tsx";
 import Header from "../components/common/Header.tsx";
-import UpdatedUs from "../components/contactus/UpdatedUs.tsx";
+// import UpdatedUs from "../components/contactus/UpdatedUs.tsx";
 import Footer from "../components/common/Footer.tsx";
 import SignUp from "../components/common/SignUp.tsx";
 
@@ -18,7 +18,7 @@ const ContactUs = () => {
           "Hoping to have a question answered? Curious about using Yoomi for your practice? Reach out to our team for a quick response!"
         }
       />
-      <UpdatedUs />
+      {/* <UpdatedUs /> */}
       <SignUp />
       <Footer />
     </>
