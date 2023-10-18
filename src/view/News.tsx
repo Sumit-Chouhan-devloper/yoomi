b282a38e8b3f36507a9f84bc9e8e73af81d769b5
mk0029 <mkhicher404@gmail.com>
2023-10-18 12:07:42 +0530

    solve Hamburder issue

--- src/view/News.tsx
+++ src/view/News.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import NewsHero from "../components/news/NewsHero";
 import Header from "../components/common/Header";
-import SignUp from "../components/common/SignUp";
-import Footer from "../components/common/Footer";
+// import SignUp from "../components/common/SignUp";
+// import Footer from "../components/common/Footer";
+import MainNews from "../components/news/MainNews";
 
 const News = () => {
   return (
     <>
       <Header />
       <NewsHero />
-      <SignUp />
-      <Footer />
+      <MainNews />
+      {/* <SignUp /> */}
+      {/* <Footer /> */}
     </>
   );
 };
