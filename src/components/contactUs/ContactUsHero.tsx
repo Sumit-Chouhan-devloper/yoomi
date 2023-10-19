25069aa2e2f8c13dd921f3d1455f3a4986ea0008
sukhvinder-jaat <monudhaka829@gmail.com>
2023-10-19 10:50:17 +0530

    side image pointer event none

--- src/components/contactUs/ContactUsHero.tsx
+++ src/components/contactUs/ContactUsHero.tsx
@@ -4,28 +4,28 @@ const ContactHero = () => {
   return (
     <div className="bg-light-sky sm:translate-y-[-15%] translate-y-[-35%] z-10">
       <div className="relative translate-y-[50%] z-20">
-        <div className="bg-deep-blue w-full h-[25px] absolute top-[50%] translate-y-[-50%]"></div>
+        <div className="bg-deep-blue w-full h-[25px] absolute top-[50%] translate-y-[-50%] pointer-events-none"></div>
         <div className="container xl:max-w-[1140px] 3xl:max-w-[1320px] mx-auto px-5 xl:px-0 relative z-10">
           <div className="bg-white rounded-[32px] shadow-[0px_26px_27px_0px_rgba(0,0,0,0.04)] min-h-[215px] sm:h-[340px] flex justify-center items-center flex-col relative h-[364px]">
             <img
               src={Images.commonIcon}
               alt="common-hero-img-1"
-              className="absolute bottom-0 start-0 sm:block hidden"
+              className="absolute bottom-0 start-0 sm:block hidden pointer-events-none"
             />
             <img
               src={Images.commonIcon}
               alt="common-hero-img-1"
-              className="absolute bottom-[5px] start-0 sm:hidden w-[45px] h-[43px]"
+              className="absolute bottom-[5px] start-0 sm:hidden w-[45px] h-[43px] pointer-events-none"
             />
             <img
               src={Images.commonIcon2}
               alt="common-hero-img-2"
-              className="absolute bottom-0 end-0 sm:block hidden"
+              className="absolute bottom-0 end-0 sm:block hidden pointer-events-none"
             />
             <img
               src={Images.commonIcon2}
               alt="common-hero-img-2"
-              className="absolute bottom-0 end-0 sm:hidden w-[40px] h-[43px]"
+              className="absolute bottom-0 end-0 sm:hidden w-[40px] h-[43px] pointer-events-none"
             />
             <h2 className="uppercase font-medium text-xs sm:text-sm text-deep-blue ">
               Lets work together
